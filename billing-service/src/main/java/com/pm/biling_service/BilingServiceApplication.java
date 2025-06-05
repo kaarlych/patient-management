12367365e40167bc628fa268c232c91ba7ba5dda
Karol Janowski <karoljanowski@yahoo.com>
2025-06-05 20:51:39 +0200

    rename subfolder from example to pm as a mistake

--- billing-service/src/main/java/com/pm/biling_service/BilingServiceApplication.java
+++ billing-service/src/main/java/com/pm/biling_service/BilingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.biling_service;
+package com.pm.biling_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
