985b62675d9ef79ef962e96a74118cae06da70dc
Karol Janowski <karoljanowski@yahoo.com>
2025-06-14 10:56:28 +0200

    added kafka producer to patient-service microservice

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.response.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -21,6 +22,7 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
     public List<PatientResponseDTO> getPatients() {
         return patientRepository.findAll().stream()
@@ -41,6 +43,8 @@ public class PatientService {
                 patient.getId().toString(),
                 patient.getName(),
                 patient.getEmail());
+
+        kafkaProducer.sendEvent(patient);
 
         return PatientMapper.toDTO(patient);
     }
